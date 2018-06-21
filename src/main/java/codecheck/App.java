c48098efc005ef79255ac11a8de6ee2281194268
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-21 17:30:22 +0900

    試し

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,6 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		// mを取得
-		System.out.println(args[args.length -1]);
 		int m = Integer.parseInt(args[args.length -1]);
 
 		int a;
@@ -13,6 +12,7 @@ public class App {
 		// ｍ以前の引数分処理
 		for (int i = 0, l = args.length-1; i < l; i++) {
 			// aとsを取得
+			System.out.println(args[i].substring(0,args[i].indexOf(":")-1);
 			a = Integer.parseInt(args[i].substring(0,args[i].indexOf(":")-1));
 			s = args[i].substring(args[i].indexOf(":")+1);
 
