45c7734df24f98ca2c2bd0d1c11e09b72900d163
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-21 17:21:15 +0900

    試し

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,8 +2,27 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
+		// mを取得
+		int m = Integer.parseInt(args[args.length -1]);
+
+		int a;
+		String s;
+		StringBuilder output = null;
+
+		// ｍ以前の引数分処理
+		for (int i = 0, l = args.length-1; i < l; i++) {
+			// aとsを取得
+			a = Integer.parseInt(args[i].substring(0,args[i].indexOf(":")-1));
+			s = args[i].substring(args[i].indexOf(":")+1);
+
+			// 割り切れる場合
+			if (m % a == 0) {
+				output.append(s);
+			}
+		}
+		if (output == null) {
+			System.out.println(m);
+		} else {
 			System.out.println(output);
 		}
 	}
